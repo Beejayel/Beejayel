0b1958daeaf88d37bb71b38419bc87d3c7e275c7
Beejayel <115215920+Beejayel@users.noreply.github.com>
2022-10-07 16:36:52 +0800

    Create README.cpp

---
+++ README.cpp
@@ -0,0 +1,14 @@
+/*A program that will compare two numbers and will display the higher number.*/
+
+#include<iostream>
+  using namespace std;
+  int f=0, s=0;
+  char x;
+  
+  int main()
+  {
+  system ("cls"); //to clear the screen
+  cout<<"\n\t\tA program that will compare two numbers and will display the higher number.";
+  cout<<"\n\n\tEnter the 1st number: ";
+  
+  
